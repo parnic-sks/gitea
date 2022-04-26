e48beb9afc872efd116fb4183adf1f6e607f6891
parnic <github@parnic.com>
2022-04-26 14:09:46 -0500

    Allow commit status popup on /pulls page (#19507)
    * Allow commit status popup on /pulls page
    
    The /pulls page doesn't contain a "repository" element, so the early-out here was preventing the commit status popup hook from working. However, the only thing the .repository element is being used for here is determining whether the popup should be on the right or on the left, so we don't actually need the element to exist for the hook to work.
    
    Pull request #19375 allows the statuses on /pulls pages to appear clickable, but this commit is required to make the popup actually work there.
    
    * Move commit statuses popup hook to dedicated func
    
    * Add missing import

--- web_src/js/features/repo-legacy.js
+++ web_src/js/features/repo-legacy.js
@@ -422,18 +422,6 @@ export function initRepository() {
   }
 
 
-  // Commit statuses
-  $('.commit-statuses-trigger').each(function () {
-    const positionRight = $('.repository.file.list').length > 0 || $('.repository.diff').length > 0;
-    const popupPosition = positionRight ? 'right center' : 'left center';
-    $(this)
-      .popup({
-        on: 'click',
-        lastResort: popupPosition, // prevent error message "Popup does not fit within the boundaries of the viewport"
-        position: popupPosition,
-      });
-  });
-
   // File list and commits
   if ($('.repository.file.list').length > 0 ||
     $('.repository.commits').length > 0 || $('.repository.release').length > 0) {
