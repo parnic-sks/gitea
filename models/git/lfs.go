c284ea290b5413eefd1e21abdf12005ca9b979a9
parnic-sks <chris@straykitestudios.com>
2022-07-30 18:13:25 -0500

    Associate LFS pointer on download
    Temporary measure to work around
    https://github.com/go-gitea/gitea/issues/17715

--- models/git/lfs.go
+++ models/git/lfs.go
@@ -226,7 +226,7 @@ func CountLFSMetaObjects(repoID int64) (int64, error) {
 
 // LFSObjectAccessible checks if a provided Oid is accessible to the user
 func LFSObjectAccessible(user *user_model.User, oid string) (bool, error) {
-	if user.IsAdmin {
+	if user != nil && user.IsAdmin {
 		count, err := db.GetEngine(db.DefaultContext).Count(&LFSMetaObject{Pointer: lfs.Pointer{Oid: oid}})
 		return count > 0, err
 	}
